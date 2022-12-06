77ab54fc09512857c811045a8066d44dca05716d
patipan-patisampita <patipan.patisampita@gmail.com>
2022-12-06 11:51:06 +0700

    update

--- lib/card_demo.dart
+++ lib/card_demo.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+class Menu {
+  final String? title;
+  final IconData? icon;
+  final Color? bkColor;
+
+  const Menu({this.title, this.icon, this.bkColor});
+}
+
+List<Menu> menus = [
+  const Menu(title: 'MENU-1', icon: Icons.person, bkColor: Colors.orangeAccent),
+  const Menu(title: 'MENU-2', icon: Icons.person_add, bkColor: Colors.blue),
+  const Menu(title: 'MENU-3', icon: Icons.person_off, bkColor: Colors.red),
+  const Menu(title: 'MENU-4', icon: Icons.wifi, bkColor: Colors.green),
+  const Menu(title: 'MENU-5', icon: Icons.volcano, bkColor: Colors.purple),
+];
+
 class CardDemo extends StatelessWidget {
   const CardDemo({Key? key}) : super(key: key);
 
@@ -12,17 +28,20 @@ class CardDemo extends StatelessWidget {
       body: Container(
         child: GridView.count(
           crossAxisCount: 3,
-          children: List.generate(6, (index) {
+          children: List.generate(menus.length, (index) {
             return Card(
+              color: menus[index].bkColor,
               margin: EdgeInsets.all(12),
               child: InkWell(
-                onTap: () {},
+                onTap: () {
+                  debugPrint(menus[index].title);
+                },
                 child: Center(
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      Icon(Icons.person, size: 40, color: Colors.orangeAccent),
-                      Text("home",style: TextStyle(fontSize: 15, color: Colors.black)),
+                      Icon(menus[index].icon, size: 40, color: Colors.white),
+                      Text(menus[index].title!,style: TextStyle(fontSize: 15, color: Colors.white)),
                     ],
                   ),
                 ),
